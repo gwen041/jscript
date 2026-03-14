2189df525d5203cda7c3080bc4f87d6d51331eb6
gwen041 <almiratabios01@gmail.com>
2026-03-15 03:29:28 +0800

    adding same type

---
+++ Exercise05/script.js
@@ -0,0 +1,23 @@
+let isOpen = true;
+let isAdmin = false;
+let b = isOpen + isAdmin;
+
+let num1 = 14;
+let num2 = 56;
+let n = num1 + num2;
+
+let bi1 = 12000n;
+let bi2 = 1300n;
+let bi = bi1 + bi2;
+
+let str1 = 'jes';
+let str2 = 'gwen';
+let str = str1 + str2;
+
+let a = undefined + undefined;
+
+console.log(`${b} [${typeof b}]`);
+console.log(`${n} [${typeof n}]`);
+console.log(`${bi} [${typeof bi}]`);
+console.log(`${str} [${typeof str}]`);
+console.log(`${a} [${typeof a}]`);
