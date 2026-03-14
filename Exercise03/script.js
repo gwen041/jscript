2d9969fffa7b5cb60f0670b0364d30f6b70d6c1e
gwen041 <almiratabios01@gmail.com>
2026-03-15 02:12:20 +0800

    fix save

---
+++ Exercise03/script.js
@@ -0,0 +1,22 @@
+let contactName;
+let phone;
+let email;
+
+contactName = "Maxwell Wright";
+phone = "(0191) 719 6495";
+email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+console.log(contactName + "/" + phone + "/" + email);
+
+
+contactName = "Raja Villareal";
+phone = "0866 398 2895";
+email = "posuere.vulputate@sed.com";
+
+console.log(contactName + "/" + phone + "/" + email);
+
+contactName = "Helen Richards";
+phone = "0800 1111";
+email = "libero@convallis.edu";
+
+console.log(contactName + "/" + phone + "/" + email);
