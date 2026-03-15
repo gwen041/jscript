d75ff28f522147d0248083f6732af5657e4c27fd
gwen041 <almiratabios01@gmail.com>
2026-03-15 16:46:40 +0800

    using array methods

--- Exercise06/script2.js
+++ Exercise06/script2.js
@@ -44,3 +44,26 @@ console.log(`Book 1 Title: ${books[0].title}`);
 console.log(`Book 2 Title: ${books[1].title}`);
 console.log(`Book 3 Title: ${books[2].title}`);
 console.log(`Book 4 Title: ${books[3].title}`);
+
+let last2Books = books.slice(-2);
+
+console.log(last2Books);
+
+let lostFirstBook = books.shift();
+
+console.log(lostFirstBook);
+console.log(books.length);
+
+displayTitle();
+
+let book1Pages = books[0].pages;
+let book2Pages = books[1].pages;
+let book3Pages = books[2].pages;
+
+let sumPages = book1Pages + book2Pages + book3Pages;
+
+
+console.log(`BOOK 1 Pages: ${book1Pages}`);
+console.log(`BOOK 2 Pages: ${book2Pages}`);
+console.log(`BOOK 3 Pages: ${book3Pages}`);
+console.log(`TOTAL Pages: ${sumPages}`);
